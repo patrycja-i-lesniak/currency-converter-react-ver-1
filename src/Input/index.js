cf61e0eeb1b17fd5636526586299dd910c4fb012
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-13 14:18:57 +0200

    Input interaction added

--- src/Input/index.js
+++ src/Input/index.js
@@ -1,13 +1,21 @@
 import "./style.css";
+import { useState } from "react";
 
-const Input = () => (
-    <input
-        className="input"
-        type="number"
-        min="0.01"
-        step="0.01"
-        placeholder="0,00"
-        required />
-);
+const Input = () => {
+    const [inputValue, setInputValue] = useState("");
+
+    return (
+        <input
+            className="input"
+            type="number"
+            min="0.01"
+            step="0.01"
+            placeholder="0,00"
+            required
+            value={inputValue}
+            onChange={({ target }) => setInputValue(target.value)}
+        />
+    )
+};
 
 export default Input;
