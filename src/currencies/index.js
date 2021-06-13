2a54add89a6af5ad9e811cc5d6e4c5a9435fd328
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-13 14:19:51 +0200

    currencies element changed

--- src/currencies/index.js
+++ src/currencies/index.js
@@ -1,7 +1,6 @@
 const currencies = [
-    { id: 1, name: "wybierz walutę" },
-    { id: 2, name: "CHF - Frank szwajcarski", exchangeRate: 4.1368 },
-    { id: 3, name: "EUR - Euro", exchangeRate: 4.5427 },
-    { id: 4, name: "GBP - Brytyjski funt szterling", exchangeRate: 5.2250 },
+    { id: 1, name: "CHF - Frank szwajcarski", exchangeRate: 4.1368 },
+    { id: 2, name: "EUR - Euro", exchangeRate: 4.5427 },
+    { id: 3, name: "GBP - Brytyjski funt szterling", exchangeRate: 5.2250 },
 ];
 export default currencies;
