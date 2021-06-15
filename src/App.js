109a95e103499289cdb905fb092f9d9f9434ed6d
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-15 09:16:41 +0200

    calculateResult function added, useState aded

--- src/App.js
+++ src/App.js
@@ -2,19 +2,41 @@ import Header from "./Header";
 import Form from "./Form";
 import Footer from "./Footer";
 import Container from "./Container";
-import currencies from "./currencies";
+import { useState } from "react";
+import currencies from "./currencies"
 
 
 function App() {
+  const [result, setResult] = useState();
+
+  const calculateResult = (amount, currency) => {
+    const rate = currencies
+      .find(({ id }) => id === currency)
+      .rate;
+
+    setResult({
+      sourceAmount: +amount,
+      targetAmount: amount / rate,
+      currency,
+    });
+  }
+
   return (
     <>
-    <Container>
-      <Header title="Kalkulator walut" />
-      <Form currencies={currencies} />
-      <Footer text="Coded by Trishya © 2021" />
-    </Container>
+      <Container>
+        <Header
+          title="Kalkulator walut"
+        />
+        <Form
+          calculateResult={calculateResult}
+          result={result}
+        />
+        <Footer
+          text="Coded by Trishya © 2021"
+        />
+      </Container>
     </>
   );
-}
+};
 
 export default App;
