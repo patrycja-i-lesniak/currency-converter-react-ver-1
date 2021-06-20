e1a3df5b9b104c9c4e9ef716503899e8de71d62e
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-20 11:34:22 +0200

    formattedDate added

--- src/Form/Clock/index.js
+++ src/Form/Clock/index.js
@@ -2,29 +2,33 @@ import { useState, useEffect } from "react";
 import "./style.css";
 
 const Clock = () => {
-    const [date, setDate] = useState("");
+    const [date, setDate] = useState(new Date());
 
     useEffect(() => {
         const intervalId = setInterval(() => {
-            setDate(new Date().toLocaleDateString(undefined,
-                {
-                    weekday: "long",
-                    day: "numeric",
-                    month: "long",
-                    hour: "numeric",
-                    minute: "numeric",
-                    second: "numeric",
-                }
-            ));
-        }, 0);
+            setDate(new Date());
+        }, 1000);
 
         return () => {
             clearInterval(intervalId)
         };
     });
+
+    const formattedDate = date.toLocaleDateString(undefined,
+        {
+            weekday: "long",
+            day: "numeric",
+            month: "long",
+            hour: "numeric",
+            minute: "numeric",
+            second: "numeric",
+        }
+    );
+
     return (
-        <p className="clock">Dzisiaj jest {date}</p>
-    )
+        <p className="clock">Dzisiaj jest {formattedDate}</p>
+    );
+
 };
 
 export default Clock;
