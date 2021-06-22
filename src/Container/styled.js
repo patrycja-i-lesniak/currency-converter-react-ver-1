d0c8e1ed76a7723a8772050007531892299be8dd
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-22 21:38:53 +0200

    styled components added to Container

---
+++ src/Container/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Main = styled.mani`
+    max-width: 650px;
+    margin: 20px auto;
+    box-shadow: 5px 5px 10px #ccc;
+    font-family: "Lato", sans-serif;
+    min-width: 280px;
+
+    @media (max-width: 720px) {
+      margin: 15px;
+    }
+`;
+
+
+
