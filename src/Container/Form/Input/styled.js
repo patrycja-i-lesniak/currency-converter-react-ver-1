31f759655e70ec73528b167f7e3e5a5b6c9a155c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-06-24 09:45:29 +0200

    code formatted

--- src/Container/Form/Input/styled.js
+++ src/Container/Form/Input/styled.js
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
 export const InputBody = styled.input`
-  padding: 20px;
-  margin: 0 30px;
-  border: 1px solid #ddd;
-  color: hsl(0, 0%, 60%);
-  box-shadow: ${({ theme }) => theme.shadow};
+    padding: 20px;
+    margin: 0 30px;
+    border: 1px solid #ddd;
+    color: hsl(0, 0%, 60%);
+    box-shadow: ${({ theme }) => theme.shadow};
 `;
