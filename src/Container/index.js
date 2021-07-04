e21ab54e3a27a270a92c92142e414801542c7e13
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-04 20:23:26 +0200

    max-width changed to 450px, new background color

--- src/Container/index.js
+++ src/Container/index.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.main`
-    max-width: 650px;
+    max-width: 450px;
     margin: 15px auto;
     box-shadow: ${({ theme }) => theme.shadow};
     font-family: "Lato", sans-serif;
     min-width: 280px;
+    background-color: ${({ theme }) => theme.colors.selectiveYellow};
 
     @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
       margin: 15px;
