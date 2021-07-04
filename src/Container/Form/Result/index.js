d06092e73f5f6aed925162b2ea554cbd0d24246c
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-04 16:47:22 +0200

    text in paragraph changed, date aded to props

--- src/Container/Form/Result/index.js
+++ src/Container/Form/Result/index.js
@@ -1,11 +1,11 @@
 import { Wrapped, Paragraph } from "./styled.js";
 
-const Result = ({ result }) => (
+const Result = ({ result, date }) => (
     <Wrapped>
         {result !== undefined && (
             <>
                 <Paragraph>Kursy walut pobierane są z Europejskiego BankuCentralnego. </Paragraph>
-                <Paragraph> Aktualne na dzień:</Paragraph>
+                <Paragraph>Kurs aktualny na dzień: {date} </Paragraph>
                 <Paragraph bolder>
                     {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;={" "}
                     {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
