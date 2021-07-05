411a9f241f894872a02199d11569438004a83b33
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-05 17:50:14 +0200

    color and opacity changed

--- src/Container/Form/Clock/styled.js
+++ src/Container/Form/Clock/styled.js
@@ -4,9 +4,8 @@ export const Paragraph = styled.p`
     text-align: right;
     font-family: "Source Code Pro", monospace;
     font-size: 12px;
-    color: ${({ theme }) => theme.colors.selectiveYellow};
+    color: ${({ theme }) => theme.colors.crimson};
     margin-right: 10px;
-    opacity: 50%;
 
     @media (max-width: ${({ theme }) => theme.breakpoints.small}px) {
       text-align: center;
