db386e528f18832ec327754cce468359131f09db
Patrycja Leśniak <patrycja.lesniak@gmail.com>
2021-07-05 21:27:56 +0200

    paragraph deleted

--- src/Container/Form/Result/index.js
+++ src/Container/Form/Result/index.js
@@ -4,12 +4,11 @@ const Result = ({ result, date }) => (
     <Wrapped>
         {result !== undefined && (
             <>
-                <Paragraph result>Kursy walut pobierane są z Europejskiego Banku Centralnego. </Paragraph>
-                <Paragraph result>Kurs aktualny na dzień: {date} </Paragraph>
                 <Paragraph highlight>
                     {result.sourceAmount.toFixed(2)}&nbsp;&nbsp;PLN&nbsp;&nbsp;={"  "}
                     {result.targetAmount.toFixed(2)}&nbsp;&nbsp;{result.currency}
                 </Paragraph>
+                <Paragraph result>Kurs aktualny na dzień: {date} </Paragraph>
             </>
         )}
     </Wrapped>
